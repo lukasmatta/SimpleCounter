59b0affa12dd7081ce6d7e1acda37714b38f09db
Lukas Matta <lukas@matta.sk>
2020-02-28 21:41:57 +0100

    Initial Commit

---
+++ SimpleCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCounter
+//
+//  Created by Lukáš Matta on 28/02/2020.
+//  Copyright © 2020 Lukáš Matta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
